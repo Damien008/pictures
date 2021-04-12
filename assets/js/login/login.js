0ac87e6e127f5b5404097764c9335b857b58286f
Damien008 <damien.lambert999@gmail.com>
2021-04-12 17:02:32 +0200

    ajout css login

--- assets/js/login/login.js
+++ assets/js/login/login.js
@@ -6,9 +6,9 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import '../../styles/login/login.scss';
 
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // start the Stimulus application
-import './bootstrap';
+import '../../bootstrap';
