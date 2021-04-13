f8755aca9ca5ca38e6fe13033d7491df89faacdc
Damien008 <damien.lambert999@gmail.com>
2021-04-13 09:29:51 +0200

    ajout des pages actualites et réalisation + pagination

---
+++ src/Controller/BlogpostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BlogpostRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BlogpostController extends AbstractController
+{
+    /**
+     * @Route("/actualites", name="actualites")
+     */
+    public function actualites(
+        BlogpostRepository $blogpostRepository,
+        PaginatorInterface $paginatorInterface,
+        Request $request
+    ): Response {
+        $data = $blogpostRepository->findAll();
+
+        $blogposts = $paginatorInterface->paginate($data, $request->query->getInt('page', 1), 6);
+
+        return $this->render('blogpost/actualites.html.twig', [
+            'blogposts' => $blogposts,
+        ]);
+    }
+}
