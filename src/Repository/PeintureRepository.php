30720c4d85958b222185f4b294423d0151148006
Damien008 <damien.lambert999@gmail.com>
2021-06-08 10:04:39 +0200

    ajout portfolio

--- src/Repository/PeintureRepository.php
+++ src/Repository/PeintureRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Categorie;
 use App\Entity\Peinture;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -24,6 +25,16 @@ class PeintureRepository extends ServiceEntityRepository
         return $this->createQueryBuilder('p')
             ->orderBy('p.id', 'DESC')
             ->setMaxResults(3)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findAllPortfolio(Categorie $categorie)
+    {
+        return $this->createQueryBuilder('p')
+            ->where(':categorie MEMBER OF p.categorie')
+            ->andWhere('p.portfolio = true')
+            ->setParameter('categorie', $categorie)
             ->getQuery()
             ->getResult();
     }
