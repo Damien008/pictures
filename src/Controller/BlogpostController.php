eba858bb006cb7d1721fd1e53b85ac1cfc51d8f4
Damien008 <damien.lambert999@gmail.com>
2021-06-08 15:17:49 +0200

    ajout du détail d'un blogpost

--- src/Controller/BlogpostController.php
+++ src/Controller/BlogpostController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Blogpost;
 use App\Repository\BlogpostRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,6 +26,16 @@ class BlogpostController extends AbstractController
 
         return $this->render('blogpost/actualites.html.twig', [
             'blogposts' => $blogposts,
+        ]);
+    }
+
+    /**
+     * @Route("/actualites/{slug}", name="actualites_detail")
+     */
+    public function detail(Blogpost $blogpost): Response
+    {
+        return $this->render('blogpost/detail.html.twig', [
+            'blogpost' => $blogpost
         ]);
     }
 }
